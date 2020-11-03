2e31edaed1c2aeb6ee77c02cf3fc584062309a2a
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-03 08:04:30 -0600

    Create Footer component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import Footer from './components/footer';
+import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
-  return <JumbotronContainer />;
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  );
 }
